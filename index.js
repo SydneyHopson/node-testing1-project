18087a4718f4a1acbaabf91ba5e8b46f55441309
Sydney.Hopson <Karatesyd@icloud.com>
2023-06-13 12:30:43 -0400

    wip

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ class Car {
   constructor(name, tankSize, mpg) {
     this.odometer = 0 // car initilizes with zero miles
     this.tank = tankSize // car initiazes full of gas
-    // ✨ initialize whatever other properties are needed
+    this.tankSize = tankSize;
   }
 
   /**
@@ -133,7 +133,9 @@ class Car {
    * focus.drive(200) // returns 600 (ran out of gas after 100 miles)
    */
   drive(distance) {
-    // ✨ implement
+   this.odometer = this.odometer + distance;
+   return this.odometer;
+
   }
 
   /**
